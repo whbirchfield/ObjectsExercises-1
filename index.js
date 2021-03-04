1e09bdf54b55a9b8de1376cdaac9098d77ac41ee
Will B <willhbjr@gmail.com>
2021-03-04 04:29:36 +0000

    added objects to array

--- index.js
+++ index.js
@@ -1,63 +1,88 @@
 let superChimpOne = {
-    name: "Chad",
-    species: "Chimpanzee",
-    mass: 9,
-    age: 6,
-    astronautID: 1,
-    move: function() {return Math.floor(Math.random()*11)}
+  name: "Chad",
+  species: "Chimpanzee",
+  mass: 9,
+  age: 6,
+  astronautID: 1,
+  move: function() { return Math.floor(Math.random() * 11) }
 };
 
 let salamander = {
-    name: "Lacey",
-    species: "Axolotl Salamander",
-    mass: 0.1,
-    age: 5,
-    astronautID: 2,
-    move: function() {return Math.floor(Math.random()*11)}
+  name: "Lacey",
+  species: "Axolotl Salamander",
+  mass: 0.1,
+  age: 5,
+  astronautID: 2,
+  move: function() { return Math.floor(Math.random() * 11) }
 };
 
 let superChimpTwo = {
-    name: "Brad",
-    species: "Chimpanzee",
-    mass: 11,
-    age: 6,
-    astronautID: 10,
-    move: function() {return Math.floor(Math.random()*11)}
+  name: "Brad",
+  species: "Chimpanzee",
+  mass: 11,
+  age: 6,
+  astronautID: 10,
+  move: function() { return Math.floor(Math.random() * 11) }
 };
 
 let superCanine = {
-    name: "Leroy",
-    species: "Beagle",
-    mass: 14,
-    age: 5,
-    astronautID: 3,
-    move: function() {return Math.floor(Math.random()*11)}
+  name: "Leroy",
+  species: "Beagle",
+  mass: 14,
+  age: 5,
+  astronautID: 3,
+  move: function() { return Math.floor(Math.random() * 11) }
 };
 
 let superInsect = {
-    name: "Almina",
-    species: "Tardigrade",
-    mass: 0.0000000001,
-    age: 1,
-    astronautID: 4,
-    move: function() {return Math.floor(Math.random()*11)}
+  name: "Almina",
+  species: "Tardigrade",
+  mass: 0.0000000001,
+  age: 1,
+  astronautID: 4,
+  move: function() { return Math.floor(Math.random() * 11) }
 };
 
-let crew = ['superChimpOne', 'salamander', 'superChimpTwo', 'superCanine', 'superInsect' ];
+let crew = [{name: "Chad",
+  species: "Chimpanzee",
+  mass: 9,
+  age: 6,
+  astronautID: 1,
+  move: function() { return Math.floor(Math.random() * 11) }}, {name: "Lacey",
+  species: "Axolotl Salamander",
+  mass: 0.1,
+  age: 5,
+  astronautID: 2,
+  move: function() { return Math.floor(Math.random() * 11) }},{name: "Brad",
+  species: "Chimpanzee",
+  mass: 11,
+  age: 6,
+  astronautID: 10,
+  move: function() { return Math.floor(Math.random() * 11) }}, {name: "Leroy",
+  species: "Beagle",
+  mass: 14,
+  age: 5,
+  astronautID: 3,
+  move: function() { return Math.floor(Math.random() * 11) }}, {name: "Almina",
+  species: "Tardigrade",
+  mass: 0.0000000001,
+  age: 1,
+  astronautID: 4,
+  move: function() { return Math.floor(Math.random() * 11) }}];
 
 
 
-for (item in superChimpOne){
+for (item in superChimpOne) {
   console.log(item + ", " + superChimpOne[item]);
 }
 
 console.log('\n');
-for (item in salamander){
+for (item in salamander) {
   console.log(item + ", " + salamander[item]);
 }
 
 
-function crewReports ( for in animalObjects){
+function crewReports(animalObjects) {
 
   let sentence = (`${animalObjects["name"]} is a ${animalObjects["species"]}. They are ${animalObjects["age"]} years old and ${animalObjects["mass"]} kilograms. Their ID is ${animalObjects["astronautID"]}.`)
   return sentence;
